fde30717535266fe9823594f976d143e3c1275bb
Kate Humphrey <khumphrey7890@gmail.com>
2017-11-27 18:10:34 -0500

    Update index.js
    so that they actually have to code something

--- client/index.js
+++ client/index.js
@@ -3,9 +3,5 @@
 //   An export named `events`, calling it `whiteboard`.
 import whiteboard, {draw} from './whiteboard'
 
-// Example of listening to draw events:
-//   (This logging will probably get really annoying):
-whiteboard.on('draw', console.log)
-
 // Example: Draw a single stroke.
 draw([0, 0], [250, 250], 'red', true)
