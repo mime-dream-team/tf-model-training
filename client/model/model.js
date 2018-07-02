2f9e259d6fcdd2a7b9735831e787ec9e937f856a
Chiara Marcial Martínez <cmarmar@gmail.com>
2018-07-02 18:39:29 -0400

    prepped with relu on output layer

--- client/model/model.js
+++ client/model/model.js
@@ -5,31 +5,31 @@ import * as tf from '@tensorflow/tfjs'
 const model = tf.sequential()
 
 model.add(
-  tf.layers.dense({
-    inputShape: [20],
-    activation: 'sigmoid',
-    units: 21
-  })
+	tf.layers.dense({
+		inputShape: [20],
+		activation: 'relu',
+		units: 21
+	})
 )
 
 model.add(
-  tf.layers.dense({
-    inputShape: [21],
-    activation: 'sigmoid',
-    units: 3
-  })
+	tf.layers.dense({
+		inputShape: [21],
+		activation: 'relu',
+		units: 3
+	})
 )
 
 model.add(
-  tf.layers.dense({
-    activation: 'sigmoid',
-    units: 3
-  })
+	tf.layers.dense({
+		activation: 'linear',
+		units: 3
+	})
 )
 
 model.compile({
-  loss: 'meanSquaredError',
-  optimizer: tf.train.adam(0.06)
+	loss: 'meanSquaredError',
+	optimizer: tf.train.adam(0.06)
 })
 
 export default model
