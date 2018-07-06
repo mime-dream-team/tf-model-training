edd29136a229a57f50db9169e8816a86f94a3ca3
Lizz Thabet <lizzthabet@gmail.com>
2018-07-06 10:45:15 -0400

    fix(fire): update fire configuration

--- client/fire/fire.js
+++ client/fire/fire.js
@@ -1,14 +1,6 @@
 import fire from 'firebase'
+import { config } from '../fireconfig'
 
-const config = {
-    apiKey: "AIzaSyBFPIdZzDtOGLvSFu_lXXDqiv6IwPWhhso",
-    authDomain: "mime-wireframes.firebaseapp.com",
-    databaseURL: "https://mime-wireframes.firebaseio.com",
-    projectId: "mime-wireframes",
-    storageBucket: "",
-    messagingSenderId: "112640428638"
-  };
+fire.initializeApp(config)
 
-fire.initializeApp(config);
-
-export default fire;
+export default fire
